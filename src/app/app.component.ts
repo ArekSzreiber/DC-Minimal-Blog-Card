bcbab6817ff929ac3179959fceffd21ec50f0518
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2024-02-05 17:47:56 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   template: `
-    <h1>Welcome to {{title}}!</h1>
 
-    <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'Minimal-Blog-Card';
 }
